7c9dae174fca46c227cd932792195614be5652d3
Angelo <angjelin003@gmail.com>
2026-09-05 12:20:52 +0200

    Implement immersive fullscreen mode in MainActivity
    Added immersive fullscreen mode to MainActivity.

--- app/src/main/java/com/editor/MainActivity.kt
+++ app/src/main/java/com/editor/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.editor
+ package com.editor
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,6 +22,14 @@ import com.editor.ui.theme.EditorTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Enable immersive fullscreen mode (hides status & navigation bars)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
         setContent {
             EditorTheme {
                 // A surface container using the 'background' color from the theme
