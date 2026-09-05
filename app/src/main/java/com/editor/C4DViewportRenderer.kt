927615d6bf61ba0da8712bedf24c767fcd14a53a
Angelo <angjelin003@gmail.com>
2026-09-05 09:51:18 +0200

    Add C4DViewportRenderer for OpenGL rendering

---
+++ app/src/main/java/com/editor/C4DViewportRenderer.kt
@@ -0,0 +1,34 @@
+package com.editor
+
+import android.opengl.GLES20
+import android.opengl.GLSurfaceView
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
+class C4DViewportRenderer : GLSurfaceView.Renderer {
+
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        // C4D Default Dark Grey Background
+        GLES20.glClearColor(0.25f, 0.25f, 0.25f, 1.0f)
+        
+        // Enable Depth Testing for 3D Objects
+        GLES20.glEnable(GLES20.GL_DEPTH_TEST)
+        
+        // Enable Blending for half-transparent wireframe grid
+        GLES20.glEnable(GLES20.GL_BLEND)
+        GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA)
+    }
+
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        GLES20.glViewport(0, 0, width, height)
+        // Setup Perspective Matrix (Projection) here
+    }
+
+    override fun onDrawFrame(gl: GL10?) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT)
+        
+        // 1. Draw Viewport Grid (Half-transparent lines)
+        // 2. Apply Camera View Matrix (Pan, Zoom, Orbit)
+        // 3. Draw Scene Objects (Cube, Sphere, .obj files)
+    }
+}
