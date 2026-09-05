9c756107931b974efded1bdba4b3cf76c211c10a
Angelo <angjelin003@gmail.com>
2026-09-05 10:49:14 +0200

    Clean up comments in createCube function
    Removed comments describing cube faces and normals.

--- app/src/main/java/com/editor/PrimitiveBuilder.kt
+++ app/src/main/java/com/editor/PrimitiveBuilder.kt
@@ -3,45 +3,32 @@ package com.editor
 object PrimitiveBuilder {
     
     fun createCube(): MeshData {
-        // A cube has 6 faces, 2 triangles per face = 36 vertices
         val v = floatArrayOf(
-            // Front face
             -1f, -1f,  1f,   1f, -1f,  1f,   1f,  1f,  1f,
             -1f, -1f,  1f,   1f,  1f,  1f,  -1f,  1f,  1f,
-            // Right face
              1f, -1f,  1f,   1f, -1f, -1f,   1f,  1f, -1f,
              1f, -1f,  1f,   1f,  1f, -1f,   1f,  1f,  1f,
-            // Back face
              1f, -1f, -1f,  -1f, -1f, -1f,  -1f,  1f, -1f,
              1f, -1f, -1f,  -1f,  1f, -1f,   1f,  1f, -1f,
-            // Left face
             -1f, -1f, -1f,  -1f, -1f,  1f,  -1f,  1f,  1f,
             -1f, -1f, -1f,  -1f,  1f,  1f,  -1f,  1f, -1f,
-            // Top face
             -1f,  1f,  1f,   1f,  1f,  1f,   1f,  1f, -1f,
             -1f,  1f,  1f,   1f,  1f, -1f,  -1f,  1f, -1f,
-            // Bottom face
             -1f, -1f, -1f,   1f, -1f, -1f,   1f, -1f,  1f,
             -1f, -1f, -1f,   1f, -1f,  1f,  -1f, -1f,  1f
         )
 
         val n = floatArrayOf(
-            // Front face normals (Z+)
              0f, 0f, 1f,   0f, 0f, 1f,   0f, 0f, 1f,
              0f, 0f, 1f,   0f, 0f, 1f,   0f, 0f, 1f,
-            // Right face normals (X+)
              1f, 0f, 0f,   1f, 0f, 0f,   1f, 0f, 0f,
              1f, 0f, 0f,   1f, 0f, 0f,   1f, 0f, 0f,
-            // Back face normals (Z-)
              0f, 0f, -1f,  0f, 0f, -1f,  0f, 0f, -1f,
              0f, 0f, -1f,  0f, 0f, -1f,  0f, 0f, -1f,
-            // Left face normals (X-)
             -1f, 0f, 0f,  -1f, 0f, 0f,  -1f, 0f, 0f,
             -1f, 0f, 0f,  -1f, 0f, 0f,  -1f, 0f, 0f,
-            // Top face normals (Y+)
              0f, 1f, 0f,   0f, 1f, 0f,   0f, 1f, 0f,
              0f, 1f, 0f,   0f, 1f, 0f,   0f, 1f, 0f,
-            // Bottom face normals (Y-)
              0f, -1f, 0f,  0f, -1f, 0f,  0f, -1f, 0f,
              0f, -1f, 0f,  0f, -1f, 0f,  0f, -1f, 0f
         )
